c24d4bb34597c5bb2b5739091de18441149397da
IsaacT6 <isaactorresidt@gmail.com>
2022-01-18 20:50:42 -0800

    Signed-off-by: IsaacT6 <isaactorresidt@gmail.com>

---
+++ hwk/Hello World/CppApplication_1/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: isaac
+ *
+ * Created on January 18, 2022, 8:43 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << " Hello World! ";
+    return 0;
+}
+
